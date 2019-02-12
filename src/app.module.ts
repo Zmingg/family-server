e13ed8d1d0a75fffdf3a772442d0cab0869931b6
zhaiming <zhaiming@aecg.com.cn>
2019-02-12 18:31:16 +0800

    interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import ResponseMiddleware from './middleware/response.middleware';
 import PassportModule from './modules/passport/passport.module';
 import ArticleModule from './modules/article/article.module';
 
@@ -9,4 +10,11 @@ import ArticleModule from './modules/article/article.module';
     PassportModule, ArticleModule
   ]
 })
-export class ApplicationModule {}
+
+export class ApplicationModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ResponseMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
-import HttpException from './exceptions/http';
 import * as compression from 'compression';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.use(compression());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
