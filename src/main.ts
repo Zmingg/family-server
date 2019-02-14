d32d79282cfad5b3637d33fae4efb81ad39ee6ba
zhaiming <zhaiming@aecg.com.cn>
2019-02-14 15:14:17 +0800

    Auth Guard

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 import * as compression from 'compression';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { AuthGuard } from './guards/auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.use(compression());
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalGuards(new AuthGuard());
   await app.listen(3000);
 }
 bootstrap();
